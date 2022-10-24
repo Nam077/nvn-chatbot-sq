a794a13933f04858c3a5bf41ec62076fb753875f
nam077 <nvnam.18it5@vku.udn.vn>
2022-10-24 11:01:01 +0700

    update messenger service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -12,11 +12,9 @@ class AuthService {
   public users = DB.Users;
 
   public async signup(userData: CreateUserDto): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
+    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");:q
     const findUser: User = await this.users.findOne({ where: { email: userData.email } });
     if (findUser) throw new HttpException(409, `You're email ${userData.email} already exists`);
-
     const hashedPassword = await hash(userData.password, 10);
     const createUserData: User = await this.users.create({ ...userData, password: hashedPassword });
 
